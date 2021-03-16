7966a8eba1eec4a296911ac7007c84c80e7c5031
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-16 10:57:30 -0300

    dev

--- src/main/kotlin/com/redefantasy/proxy/misc/punish/command/RevokeCommand.kt
+++ src/main/kotlin/com/redefantasy/proxy/misc/punish/command/RevokeCommand.kt
@@ -56,7 +56,7 @@ class RevokeCommand : CustomCommand("despunir"), GroupCommandRestrictable {
             return false
         }
 
-        if (!userPunishment.isActive()) {
+        if (!userPunishment.isActive() && userPunishment.startTime !== null) {
             commandSender.sendMessage(TextComponent("§cEsta punição não está mais ativa."))
             return false
         }
