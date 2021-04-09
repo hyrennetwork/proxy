ec0970a426537b6d051527319047f8777a93f921
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-09 11:31:35 -0300

    dev

--- src/main/kotlin/com/redefantasy/proxy/listeners/connection/PostLoginListener.kt
+++ src/main/kotlin/com/redefantasy/proxy/listeners/connection/PostLoginListener.kt
@@ -1,12 +1,7 @@
 package com.redefantasy.proxy.listeners.connection
 
-import com.redefantasy.core.shared.CoreProvider
-import com.redefantasy.core.shared.misc.skin.Skin
-import com.redefantasy.core.shared.misc.skin.controller.SkinController
 import net.md_5.bungee.api.event.PostLoginEvent
 import net.md_5.bungee.api.plugin.Listener
-import net.md_5.bungee.connection.InitialHandler
-import net.md_5.bungee.connection.LoginResult
 import net.md_5.bungee.event.EventHandler
 
 /**
@@ -18,32 +13,32 @@ class PostLoginListener : Listener {
 	fun on(
 		event: PostLoginEvent
 	) {
-		val player = event.player
-		val user = CoreProvider.Cache.Local.USERS.provide().fetchById(player.uniqueId)
-
-		val initialHandler = player.pendingConnection as InitialHandler
-
-		val skin: Skin? =
-			if (user !== null && CoreProvider.Cache.Local.USERS_SKINS.provide().fetchByUserId(user.id) !== null
-				&& CoreProvider.Cache.Local.USERS_SKINS.provide().fetchByUserId(user.id)!!.isNotEmpty()
-			) {
-				CoreProvider.Cache.Local.USERS_SKINS.provide().fetchByUserId(user.id)?.stream()
-					?.filter { it.enabled }
-					?.findFirst()
-					?.orElse(null)?.skin
-			} else SkinController.fetchSkinByName(player.name)
-
-		if (skin !== null) {
-			val loginProfile = initialHandler.loginProfile
-
-			loginProfile.properties = arrayOf(
-				LoginResult.Property(
-					"textures",
-					skin.value,
-					null
-				)
-			)
-		}
+//		val player = event.player
+//		val user = CoreProvider.Cache.Local.USERS.provide().fetchById(player.uniqueId)
+//
+//		val initialHandler = player.pendingConnection as InitialHandler
+//
+//		val skin: Skin? =
+//			if (user !== null && CoreProvider.Cache.Local.USERS_SKINS.provide().fetchByUserId(user.id) !== null
+//				&& CoreProvider.Cache.Local.USERS_SKINS.provide().fetchByUserId(user.id)!!.isNotEmpty()
+//			) {
+//				CoreProvider.Cache.Local.USERS_SKINS.provide().fetchByUserId(user.id)?.stream()
+//					?.filter { it.enabled }
+//					?.findFirst()
+//					?.orElse(null)?.skin
+//			} else SkinController.fetchSkinByName(player.name)
+//
+//		if (skin !== null) {
+//			val loginProfile = initialHandler.loginProfile
+//
+//			loginProfile.properties = arrayOf(
+//				LoginResult.Property(
+//					"textures",
+//					skin.value,
+//					skin.signature
+//				)
+//			)
+//		}
 	}
 
 }
